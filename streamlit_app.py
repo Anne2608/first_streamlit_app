9b1fae140dd5c252224d070d016567738fc23abb
Mohammad Anas <88486838+Anne2608@users.noreply.github.com>
2023-09-13 11:55:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣  🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit>text('🥑🍞Avocado Toast')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('Create your Own Smoothie')
